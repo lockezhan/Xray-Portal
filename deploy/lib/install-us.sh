6dca8f0ec51dda0e468aa1d4b4f2f98cba19584c
lockezhan <ryanzhang_293@qq.com>
2026-07-11 21:18:35 +0800

    fix(deploy): 修复 Nginx 重定向规则字符串变量转义缺失导致的 unbound variable 报错

--- deploy/lib/install-us.sh
+++ deploy/lib/install-us.sh
@@ -1007,7 +1007,7 @@ server {
     ssl_certificate_key /etc/letsencrypt/live/${US_SUB_DOMAIN}/privkey.pem;
 
     # 自动处理误用 http:// 打开 HTTPS 端口的情况 (Nginx 497 状态码自动重定向为 https://)
-    error_page 497 https://$host:8083$request_uri;
+    error_page 497 https://\$host:8083\$request_uri;
 
     location / {
         proxy_pass http://127.0.0.1:8082;
